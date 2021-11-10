cc6c5f3b62ad877dfbc95f86dcd6b466a5527bab
rafaeldg2000 <73118304+rafaeldg2000@users.noreply.github.com>
2021-11-10 16:06:43 +0000

    Add files via upload

---
+++ Web/Backend/config/main-local.php
@@ -0,0 +1,40 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'GNvgIzBmU5vF5lHgYAzm8pk2hCmmfyZi',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'generators' => [ // here
+            'crud' => [ // generator name
+                'class' => 'yii\gii\generators\crud\Generator', // generator class
+                'templates' => [ // setting for our templates
+                    'yii2-adminlte3' => '@vendor/hail812/yii2-adminlte3/src/gii/generators/crud/default' // template name => path to template
+                ]
+            ]
+        ]
+    ];
+}
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+
+}
+
+return $config;
