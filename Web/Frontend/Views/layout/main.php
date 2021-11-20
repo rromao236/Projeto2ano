6e21cf48c1fb92cb0153cf65c7addfed6a517f5d
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-11-20 15:18:26 +0000

    Upload do main

---
+++ Web/Frontend/Views/layout/main.php
@@ -0,0 +1,89 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use common\widgets\Alert;
+use frontend\assets\AppAsset;
+use yii\bootstrap4\Breadcrumbs;
+use yii\bootstrap4\Html;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>" class="h-100">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="imgs/raricon.png">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body class="d-flex flex-column h-100">
+    <?php $this->beginBody() ?>
+
+    <header>
+        <?php
+        NavBar::begin([
+            'brandLabel' => Html::img('@web/imgs/rarlogo.png'),
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar navbar-expand-md border-bottom border-blue fixed-top',
+                'style' => 'background-color: #dbf3fa; font-size: 20px; color: #ff0000;',
+            ],
+        ]);
+        $menuItems = [
+            ['label' => 'Página Inicial', 'url' => ['/site/index']],
+            ['label' => 'Sobre', 'url' => ['/site/about']],
+            ['label' => 'Pacotes', 'url' => ['/pacotes/index']],
+        ];
+        if (Yii::$app->user->isGuest) {
+            $menuItems[] = ['label' => 'Registar', 'url' => ['/site/signup']];
+            $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        } else {
+            $menuItems[] = ['label' => 'Perfil', 'url' => ['/usersinfo/index']];
+            $menuItems[] = '<li>'
+
+                . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link logout']
+                )
+                . Html::endForm()
+                . '</li>';
+        }
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav ml-auto'],
+            'items' => $menuItems,
+        ]);
+        NavBar::end();
+        ?>
+    </header>
+
+    <main role="main" class="flex-shrink-0">
+        //class="container"
+        <div>
+            <br><br><br><br>
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </main>
+
+    <footer class="footer mt-auto py-3 text-muted">
+        <div class="container">
+            <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+            <p class="float-right"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
+
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage();
