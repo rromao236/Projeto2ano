787f897a643aca084d1dd9a6b684225ba66a77bc
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-11-20 15:10:52 +0000

    Atualizai o Controller

--- Web/Frontend/Controllers/PacotesController.php
+++ Web/Frontend/Controllers/PacotesController.php
@@ -15,7 +15,7 @@ use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
 use frontend\models\ContactForm;
-use backend\models\Packages;
+use app\models\Packages;
 
 /**
  * Site controller
