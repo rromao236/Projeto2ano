22385b6ba45e75b9207aea4848e9434427d1c440
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-11-20 15:17:45 +0000

    Upload das páginas mais importantes do site

---
+++ Web/Frontend/Views/site/about.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Sobre';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about container">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>O nosso projeto consiste na criação de um website em conjunto com uma aplicação movel ligados por uma API para venda de pacotes de viagem.</p>
+
+    <p>Este website foi realizado com o objetivo de concluir o curso de Programação de Sistemas de Informação da ESTG.</p>
+
+    <p>NÃO vendemos pacotes de viagem.</p>
+
+</div>
+
---
+++ Web/Frontend/Views/site/index.php
@@ -0,0 +1,42 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'RAR Travels';
+
+?>
+<div class="site-index">
+
+
+    <div class="jumbotron text-center bg-transparent" style="background-image: url(http://localhost/RARTravel/frontend/web/imgs/nuvens.png); background-size: cover">
+        <h1 class="display-4" style="color: #ffffff">Bem-vindos!</h1>
+
+        <br>
+
+        <p><a class="btn btn-lg btn-success" style="background-color: #ffffff; border: #005cbf; color: #005cbf" href="http://www.yiiframework.com">Ver pacotes</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4" style="text-align: center">
+                <h2 style="text-align: center"><?php echo $pacote1->title; ?></h2>
+                <img size="" width="400px" height="260" src="<?= yii\helpers\Url::to('@web/imgs/lisboa.jpg') ?>">
+                <p><br><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Ver detalhes &raquo;</a></p>
+            </div>
+            <div class="col-lg-4" style="text-align: center">
+                <h2 style="text-align: center"><?php echo $pacote2->title; ?></h2>
+                <img size="" width="400px" height="260" src="<?= yii\helpers\Url::to('@web/imgs/londres.jpg') ?>">
+                <p><br><a class="btn btn-outline-secondary " href="">Ver detalhes &raquo;</a></p>
+            </div>
+            <div class="col-lg-4" style="text-align: center">
+                <h2 style="text-align: center"><?php echo $pacote3->title; ?></h2>
+                <img size="" width="400px" height="260" src="<?= yii\helpers\Url::to('@web/imgs/paris.jpg') ?>">
+                <p><br><a class="btn btn-outline-secondary " href="">Ver detalhes &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
