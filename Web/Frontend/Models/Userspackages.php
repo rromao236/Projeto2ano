3461c7aba7e323c34f6d7795cf2c6ff799912b11
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-11-23 12:37:47 +0000

    Adicionei o Userspackages

---
+++ Web/Frontend/Models/Userspackages.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users_packages".
+ *
+ * @property int $id_user
+ * @property int $id_package
+ * @property string $purchasedate
+ * @property int $referencia
+ * @property float $price
+ * @property int $entity
+ * @property string $estado
+ *
+ * @property Packages $package
+ * @property User $user
+ */
+class Userspackages extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users_packages';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id_user', 'id_package', 'purchasedate', 'referencia', 'price', 'entity', 'estado'], 'required'],
+            [['id_user', 'id_package', 'referencia', 'entity'], 'integer'],
+            [['purchasedate'], 'safe'],
+            [['price'], 'number'],
+            [['estado'], 'string', 'max' => 50],
+            [['id_user', 'id_package'], 'unique', 'targetAttribute' => ['id_user', 'id_package']],
+            [['id_package'], 'exist', 'skipOnError' => true, 'targetClass' => Packages::className(), 'targetAttribute' => ['id_package' => 'id']],
+            [['id_user'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['id_user' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_user' => 'Id User',
+            'id_package' => 'Id Package',
+            'purchasedate' => 'Purchasedate',
+            'referencia' => 'Referencia',
+            'price' => 'Price',
+            'entity' => 'Entity',
+            'estado' => 'Estado',
+        ];
+    }
+
+    /**
+     * Gets query for [[Package]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPackage()
+    {
+        return $this->hasOne(Packages::className(), ['id' => 'id_package']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'id_user']);
+    }
+}
