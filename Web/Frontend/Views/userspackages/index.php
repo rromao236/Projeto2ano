04134f05a9b7367856da709c8a26ff95e029fc84
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-11-23 12:43:21 +0000

    Adicionei o userspackages/index

---
+++ Web/Frontend/Views/userspackages/index.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+
+$this->title = 'Historico';
+$this->params['breadcrumbs'][] = ['label' => 'Perfil', 'url' => ['usersinfo/index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-index">
+
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+
+    <div class="body-content container">
+        <table style="width:100%">
+            <tr align="center">
+                <th>Pacote</th>
+                <th>Data de compra</th>
+                <th>Estado</th>
+                <th>Entidade</th>
+                <th>Referencia</th>
+                <th>Valor</th>
+            </tr>
+                <?php foreach ($pacotesC AS $pacoteC){ ?>
+                    <?php foreach ($pacotes as $pacote){?>
+                        <tr>
+                            <td align="center"><?= $pacote->title ?></td>
+                            <td align="center"><?= $pacoteC->purchasedate ?></td>
+                            <td align="center"><?= $pacoteC->estado ?></td>
+                            <td align="center"><?= $pacoteC->entity ?></td>
+                            <td align="center"><?= $pacoteC->referencia ?></td>
+                            <td align="center"><?= $pacoteC->price ?></td>
+                            <td align="center"><?= Html::a('Ver detalhes', ['', 'id' => $pacote->id], ['class' => 'btn']) ?></td>                        </tr>
+                    <?php }?>
+                <?php } ?>
+        </table>
+
+    </div>
+
+
+</div>
