d972239cdffc360d34dfab29b601dea510879f9d
rafaeldg2000 <73118304+rafaeldg2000@users.noreply.github.com>
2021-11-25 16:00:39 +0000

    Update index.php

--- Web/Frontend/Views/site/index.php
+++ Web/Frontend/Views/site/index.php
@@ -12,10 +12,8 @@ $this->title = 'RAR Travels';
 
     <div class="jumbotron text-center bg-transparent" style="background-image: url(http://localhost/RARTravel/frontend/web/imgs/nuvens.png); background-size: cover">
         <h1 class="display-4" style="color: #ffffff">Bem-vindos!</h1>
-
         <br>
-
-        <p><a class="btn btn-lg btn-success" style="background-color: #ffffff; border: #005cbf; color: #005cbf" href="http://www.yiiframework.com">Ver pacotes</a></p>
+        <?= Html::a('Ver Pacotes', ['/pacotes/index'], ['class' => 'btn btn-lg btn-success', 'style' => 'background-color: #ffffff; color: #005cbf']) ?>
     </div>
 
     <div class="body-content">
@@ -25,21 +23,21 @@ $this->title = 'RAR Travels';
                 <h2 style="text-align: center"><?php echo $pacote1->title; ?></h2>
                 <img size="" width="400px" height="260" src="<?= yii\helpers\Url::to('@web/imgs/lisboa.jpg') ?>">
                 <p></p>
-                <?= Html::a('Ver detalhes', ['', 'id' => $pacote1->id], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Ver detalhes', ['/pacotes/detalhes', 'pacote' => $pacote1->id], ['class' => 'btn btn-primary']) ?>
                 <p></p>
             </div>
             <div class="col-lg-4" style="text-align: center">
                 <h2 style="text-align: center"><?php echo $pacote2->title; ?></h2>
                 <img size="" width="400px" height="260" src="<?= yii\helpers\Url::to('@web/imgs/londres.jpg') ?>">
                 <p></p>
-                <?= Html::a('Ver detalhes', ['', 'id' => $pacote2->id], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Ver detalhes', ['/pacotes/detalhes', 'pacote' => $pacote2->id], ['class' => 'btn btn-primary']) ?>
                 <p></p>
             </div>
             <div class="col-lg-4" style="text-align: center">
                 <h2 style="text-align: center"><?php echo $pacote3->title; ?></h2>
                 <img size="" width="400px" height="260" src="<?= yii\helpers\Url::to('@web/imgs/paris.jpg') ?>">
                 <p></p>
-                <?= Html::a('Ver detalhes', ['', 'id' => $pacote3->id], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Ver detalhes', ['/pacotes/detalhes', 'pacote' => $pacote3->id], ['class' => 'btn btn-primary']) ?>
                 <p></p>
             </div>
         </div>
