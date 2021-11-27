e095caac3b6eaf261f08af2de50951bc1dbd631f
AndreSantos07 <73118292+AndreSantos07@users.noreply.github.com>
2021-11-27 16:50:34 +0000

    packageimages index atualizado

--- Web/Backend/views/packageimages/index.php
+++ Web/Backend/views/packageimages/index.php
@@ -27,6 +27,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id_image',
+            'name',
             //codigo da imagem
             //'image',
             [
