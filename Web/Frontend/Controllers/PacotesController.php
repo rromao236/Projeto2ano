c5585d8e1aeb7fae2affc4218206e7cc8ef0d9d7
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-11-27 15:21:19 +0000

    Atualizei o PacotesController

--- Web/Frontend/Controllers/PacotesController.php
+++ Web/Frontend/Controllers/PacotesController.php
@@ -71,8 +71,96 @@ class PacotesController extends Controller
         ]);
     }
 
-    public function actionCompra()
+    public function actionCompra($id_pacote)
     {
-        return $this->render('compra');
+        $session = Yii::$app->session;
+        $session->open();
+        $pac=$session['pac'];
+
+        if($pac==null){
+            $session['pac'] = $id_pacote;
+        }
+
+
+        $model = new Userspackages();
+
+        $pacote = Packages::find()
+            ->where(['id' => $id_pacote])
+            ->one();
+
+        $user = Usersinfo::find()
+            ->where(['userid' => Yii::$app->user->identity->id])
+            ->one();
+
+        if ($this->request->isPost) {
+
+            $model->load($this->request->post());
+
+            $pacoteinfo= Packages::find()
+                ->where(['id' => $pac])
+                ->one();
+
+            $userinfo = Usersinfo::find()
+                ->where(['userid' => Yii::$app->user->identity->id])
+                ->one();
+
+            $pontospossuidos=$userinfo->points;
+            $preco=$pacoteinfo->price;
+            $pessoas=$model->nrpeople;
+            $pontos=$model->usedpoints;
+
+            if($pontos>$pontospossuidos){
+                return $this->redirect(['index']);
+            } else{
+
+                $precofinal=$preco*$pessoas;
+                $desconto=$precofinal*($pontos/100);
+                $precofinal=$precofinal-$desconto;
+
+                $model->id_user=$userinfo->userid;
+                $model->id_package=$pacoteinfo->id;
+                $model->purchasedate=date('Y-m-d H:i:s');
+                $model->referencia=rand(900000000,999999999);
+                $model->price=$precofinal;
+                $model->entity=11236;
+                $model->estado="Por pagar";
+
+                $model->save();
+
+
+
+                return $this->render('view', [
+                    'model' => $model,
+                ]);
+            }
+
+
+        }
+
+
+        return $this->render('create', [
+            'pacote' => $pacote,
+            'user' => $user,
+            'model' => $model,
+        ]);
+    }
+
+    public function actionCancelar($id)
+    {
+        $this->findModel($id)->delete();
+
+        $pacotes = Packages::find()
+            ->all();
+
+        return $this->redirect('index');
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = Userspackages::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
     }
 }
