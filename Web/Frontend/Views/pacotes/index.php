4094a16a26c5298a54c749a848d18624bef8fba6
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-11-30 18:21:06 +0000

    Atualizei o pacotes/index
    Faltava codigo dentro do foreach

--- Web/Frontend/Views/pacotes/index.php
+++ Web/Frontend/Views/pacotes/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\models\Packageimages;
 use yii\helpers\Url;
 use yii\helpers\Html;
 
@@ -37,36 +38,39 @@ $this->title = 'RAR Travels';
     </style>
 
     <div class="body-content">
-        <?php foreach ($pacotes as $pacote){?>
+        <?php foreach ($pacotes as $pacote){
+            $imagem = Packageimages::find()
+                ->where(['package_id' => $pacote->id])
+                ->one();
+        ?>
             <div class="row">
                 <div class="col-lg-12" style="text-align: center;">
                     <img size="" width="420px" height="auto" align="left" style="margin-left: 100px" src="<?php echo Yii ::getAlias('@imageurl'); ?>/<?= $imagem->image?>">
                     <h3 style="text-align: left; margin-left: 540px;"><?php echo $pacote->title; ?></h3>
                     <p align="left" style="margin-left: 540px;"><?php echo $pacote->description; ?></p>
                     <p align="left" style="margin-left: 540px;">Rating: <?php switch($pacote->rating){
-                        case 0:
-                            echo "&#9734&#9734&#9734&#9734&#9734";
-                            break;
-                        case 1:
-                            echo "&#9733&#9734&#9734&#9734&#9734";
-                            break;
-                        case 2:
-                            echo "&#9733&#9733&#9734&#9734&#9734";
-                            break;
-                        case 3:
-                            echo "&#9733&#9733&#9733&#9734&#9734";
-                            break;
-                        case 4:
-                            echo "&#9733&#9733&#9733&#9733&#9734";
-                            break;
-                        case 5:
-                            echo "&#9733&#9733&#9733&#9733&#9733";
-                            break;
-                    } ?></p>
+                            case 0:
+                                echo "&#9734&#9734&#9734&#9734&#9734";
+                                break;
+                            case 1:
+                                echo "&#9733&#9734&#9734&#9734&#9734";
+                                break;
+                            case 2:
+                                echo "&#9733&#9733&#9734&#9734&#9734";
+                                break;
+                            case 3:
+                                echo "&#9733&#9733&#9733&#9734&#9734";
+                                break;
+                            case 4:
+                                echo "&#9733&#9733&#9733&#9733&#9734";
+                                break;
+                            case 5:
+                                echo "&#9733&#9733&#9733&#9733&#9733";
+                                break;
+                        } ?></p>
                     <h4 align="right" style="margin-right: 520px;">Preço: <?php echo $pacote->price; ?>€</h4>
 
                     <?= Html::a('Detalhes', ['/pacotes/detalhes', 'pacote'=>$pacote->id], ['class'=>'button button1']) ?>
-                    <!--<p align="right" style="margin-right: 100px"><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Ver detalhes &raquo;</a></p>-->
                 </div>
             </div>
         <?php }?>
