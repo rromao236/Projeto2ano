40088c1ec68abc69380bb314bcb9fb9f557b1851
AndreSantos07 <73118292+AndreSantos07@users.noreply.github.com>
2021-12-08 16:18:03 +0000

    module da api

---
+++ API/Module.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\modules\api;
+
+use Yii;
+
+/**
+ * api module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\api\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+        Yii::$app->user->enableSession = false;
+
+        // custom initialization code goes here
+    }
+}
