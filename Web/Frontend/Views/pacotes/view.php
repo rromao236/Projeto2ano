ff4425ffecfe1bcb9390b7382a54a4fc64c23ac0
rafaeldg2000 <73118304+rafaeldg2000@users.noreply.github.com>
2021-12-14 16:15:20 +0000

    Update view.php

--- Web/Frontend/Views/pacotes/view.php
+++ Web/Frontend/Views/pacotes/view.php
@@ -3,30 +3,32 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Userspackages */
 
 $session = Yii::$app->session;
 $session->destroy();
 
-$this->title = $model->id_user;
+$this->title = $pacote->title;
 $this->params['breadcrumbs'][] = ['label' => 'Userspackages', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="userspackages-view">
+    <br>
 
     <h1><?= Html::encode($this->title) ?></h1>
-
 
     <?= DetailView::widget([
 
         'model' => $model,
         'attributes' => [
-            [
+            /*[
                 'label'=>'Data de Compra',
-                'value'=>$model->purchasedate,
-            ],
+                'value'=>DateTime::createFromFormat('Y-m-d H:i:s', $model->purchasedate)->format('d-m-Y, H:i'),
+                //'value'=>DateTime::createFromFormat('Y-m-d H:i:s', $model->purchasedate)->format('d-m-Y'),
+            ],*/
             [
                 'label'=>'Pontos Usados',
                 'value'=>$model->usedpoints,
@@ -37,7 +39,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
             [
                 'label'=>'Referência',
-                'value'=>$model->referencia,
+                'value'=>$referFormat,
             ],
             [
                 'label'=>'Entidade',
@@ -45,7 +47,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
             [
                     'label'=>'Preço',
-                    'value'=>$model->price,
+                    'value'=>$model->price . "€",
             ],
         ],
     ]) ?>
