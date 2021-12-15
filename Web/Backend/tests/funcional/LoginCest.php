97ad217a5629f0a70932c5412841c589817e08df
AndreSantos07 <73118292+AndreSantos07@users.noreply.github.com>
2021-12-15 14:39:01 +0000

    teste funcional do login

---
+++ Web/Backend/tests/funcional/LoginCest.php
@@ -0,0 +1,39 @@
+<?php
+namespace backend\tests\functional;
+use backend\tests\FunctionalTester;
+class LoginCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        $I->amOnPage(['/site/login']);
+    }
+
+    protected function formParams($login, $password)
+    {
+        return [
+            'LoginForm[username]' => $login,
+            'LoginForm[password]' => $password,
+        ];
+    }
+
+    // tests
+    public function testValidLogin(FunctionalTester $I)
+    {
+        $I->submitForm('#login-form', $this->formParams('Andre', 'andre123'));
+        $I->see('Welcome to the Management page!', 'h1');
+    }
+
+    public function testInvalidLogin(FunctionalTester $I)
+    {
+        $I->submitForm('#login-form', $this->formParams('Andr', 'andre'));
+        $I->seeValidationError('Incorrect username or password.');
+        $I->dontSee('Welcome to the Management page!', 'h1');
+    }
+
+    public function testEmptyForm(FunctionalTester $I)
+    {
+        $I->submitForm('#login-form', $this->formParams('', ''));
+        $I->seeValidationError('Username cannot be blank.');
+        $I->seeValidationError('Password cannot be blank.');
+    }
+}
