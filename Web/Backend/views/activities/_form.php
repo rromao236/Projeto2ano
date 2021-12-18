376c8f9531cdb21eac2dd92e1e0eafdb5f69814d
AndreSantos07 <73118292+AndreSantos07@users.noreply.github.com>
2021-12-18 14:24:57 +0000

    update do form activities

--- Web/Backend/views/activities/_form.php
+++ Web/Backend/views/activities/_form.php
@@ -10,7 +10,7 @@ use yii\widgets\ActiveForm;
 
 <div class="activities-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['id' => 'activities-form']); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
