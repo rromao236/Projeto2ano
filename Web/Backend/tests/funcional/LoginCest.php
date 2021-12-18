fc1192712e7888870414cd2753eb7078f324b6e2
AndreSantos07 <73118292+AndreSantos07@users.noreply.github.com>
2021-12-18 14:23:37 +0000

    teste funcional do login

--- Web/Backend/tests/funcional/LoginCest.php
+++ Web/Backend/tests/funcional/LoginCest.php
@@ -30,6 +30,13 @@ class LoginCest
         $I->dontSee('Welcome to the Management page!', 'h1');
     }
 
+    public function testUserLogin(FunctionalTester $I)
+    {
+        $I->submitForm('#login-form', $this->formParams('Ricardo', 'ricardo123'));
+        $I->seeInCurrentUrl('site/login');
+        $I->dontSee('Welcome to the Management page!', 'h1');
+    }
+
     public function testEmptyForm(FunctionalTester $I)
     {
         $I->submitForm('#login-form', $this->formParams('', ''));
