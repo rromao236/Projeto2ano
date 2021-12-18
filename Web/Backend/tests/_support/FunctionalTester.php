a9f0d1998b5f9ef56c16bb8432b2e7431dcfb9ef
AndreSantos07 <73118292+AndreSantos07@users.noreply.github.com>
2021-12-18 14:31:07 +0000

    functionalTester do backend

---
+++ Web/Backend/tests/_support/FunctionalTester.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace backend\tests;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+ */
+class FunctionalTester extends \Codeception\Actor
+{
+    use _generated\FunctionalTesterActions;
+   /**
+    * Define custom actions here
+    */
+    public function seeValidationError($message)
+    {
+        $this->see($message, '.invalid-feedback');
+    }
+
+    public function dontSeeValidationError($message)
+    {
+        $this->dontSee($message, '.invalid-feedback');
+    }
+
+    public function seeValidationErrorForm($message)
+    {
+        $this->see($message, '.help-block');
+    }
+
+    public function dontSeeValidationErrorForm($message)
+    {
+        $this->dontSee($message, '.help-block');
+    }
+
+}
