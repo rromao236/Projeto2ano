cd884eba9a73926db98f317ac545ea187556b422
AndreSantos07 <73118292+AndreSantos07@users.noreply.github.com>
2021-12-21 15:34:20 +0000

    activitiesController da API atualizado

--- API/controllers/ActivitiesController.php
+++ API/controllers/ActivitiesController.php
@@ -2,8 +2,9 @@
 
 namespace app\modules\api\controllers;
 
+use app\models\Activitiespackages;
 use Yii;
-use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\QueryParamAuth;
 use yii\web\Controller;
 use app\models\Activities;
 
@@ -24,6 +25,32 @@ class ActivitiesController extends \yii\rest\ActiveController
         return $this->render('index');
     }
 
+    //recebe o id do pacote para mandar as atividades do mesmo
+    public function actionAll($id){
+        $atividades[] = null;
+        $activity = Activitiespackages::find()
+            ->where(['id_package' => $id])
+            ->all();
+        foreach ($activity as $actv){
+            $ativ = Activities::find()
+                ->where(['id' => $actv->id_activity])
+                ->one();
+
+            $atividade = new \stdClass();
+            $atividade->id = $actv->id;
+            $atividade->name = $ativ->name;
+            $atividade->idPackage = $actv->id_package;
+            $atividade->responsible = $actv->responsible;
+            $atividade->timestart = $actv->timestart;
+            $atividade->duration = $actv->duration;
+
+            $atividades[] = $atividade;
+        }
+
+        return $atividades;
+
+    }
+
     //Exemplo
     public function actionActivitie(){
         $name= Yii::$app->request->post('name');
@@ -35,20 +62,15 @@ class ActivitiesController extends \yii\rest\ActiveController
         ];
     }
 
+    /*
     public function behaviors()
     {
         $behaviors = parent::behaviors();
         $behaviors['authenticator'] = [
-            'class' => HttpBasicAuth::className(),
-            'auth' => function ($username, $password)
-            {
-                $user = \common\models\User::findByUsername($username);
-                if ($user && $user->validatePassword($password))
-                {
-                    return $user;
-                }
-            }
+            'class' => QueryParamAuth::className(),
+
         ];
         return $behaviors;
     }
+    */
 }
