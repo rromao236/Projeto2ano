332913aa97925e97546ee37a6a00f8d22db65d2a
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-12-28 14:25:06 +0000

    Atualizei o Packagesimages

--- API/controllers/PackageimagesController.php
+++ API/controllers/PackageimagesController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\api\controllers;
 
+use app\models\Packageimages;
 use yii\filters\auth\HttpBasicAuth;
 use yii\filters\auth\QueryParamAuth;
 use yii\web\Controller;
@@ -21,6 +22,22 @@ class PackageimagesController extends \yii\rest\ActiveController
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    //get packageimage
+    public function actionInformation($id){
+        $packageimage = Packageimages::find()
+            ->where(['id_image' => $id])
+            ->one();
+
+        return $packageimage;
+    }
+
+    //count packageimage
+    public function actionTotal(){
+        $climodel = new $this->modelClass;
+        $recs = $climodel::find()->all();
+        return ['total' => count($recs)];
     }
 
     public function behaviors()
