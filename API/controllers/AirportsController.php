c12257351dd217aeb4289f87af870ff5a179455e
rromao236 <73118971+rromao236@users.noreply.github.com>
2021-12-28 14:23:19 +0000

    Atualizei o AirportsController

--- API/controllers/AirportsController.php
+++ API/controllers/AirportsController.php
@@ -2,6 +2,8 @@
 
 namespace app\modules\api\controllers;
 
+use app\models\Airports;
+use Yii;
 use yii\filters\auth\HttpBasicAuth;
 use yii\filters\auth\QueryParamAuth;
 use yii\web\Controller;
@@ -23,6 +25,63 @@ class AirportsController extends \yii\rest\ActiveController
         return $this->render('index');
     }
 
+    //get airport
+    public function actionInformation($id){
+        $airport = Airports::find()
+            ->where(['id' => $id])
+            ->one();
+
+        return $airport;
+    }
+
+    //delete airport
+    public function actionApagar($id){
+        $climodel = new $this->modelClass;
+        $ret=$climodel->deleteAll("id=".$id);
+        if($ret)
+            return ['DelError' => $ret];
+        throw new \yii\web\NotFoundHttpException("Client id not found!");
+    }
+
+    //count airports
+    public function actionTotal(){
+        $climodel = new $this->modelClass;
+        $recs = $climodel::find()->all();
+        return ['total' => count($recs)];
+    }
+
+    //update airport
+    public function actionUpdate($id){
+        $name =\Yii::$app->request->post('name');
+        $country =\Yii::$app->request->post('country');
+        $city =\Yii::$app->request->post('city');
+
+        $airport = Airports::find()->where("id = ".$id)->one();
+        if($airport != null){
+            $airport->name = $name;
+            $airport->country = $country;
+            $airport->city = $city;
+            $airport->save();
+
+            return (['success' => true]);
+        }
+        return (['success' => false]);
+    }
+
+    //post airport
+    public function actionAirport(){
+        $name= Yii::$app->request->post('name');
+        $country= Yii::$app->request->post('country');
+        $city= Yii::$app->request->post('city');
+        $ativmodel = new $this->modelClass;
+        $ativmodel->name = $name;
+        $ativmodel->country = $country;
+        $ativmodel->city = $city;
+        $ret = $ativmodel->save();
+        return [
+            'SaveError' => $ret,
+        ];
+    }
 
     public function behaviors()
     {
